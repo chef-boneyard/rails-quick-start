23d7069e82a2ceefdd35291b1cda202e3412e6be
Seth Chisamore <schisamo@opscode.com>
2011-06-01 22:27:24 -0400

    put user creation as early as possible

--- roles/base.rb
+++ roles/base.rb
@@ -1,12 +1,12 @@
 name "base"
 description "Base role applied to all nodes."
 run_list(
-  "recipe[apt]",
-  "recipe[git]",
-  "recipe[build-essential]",
   "recipe[zsh]",
   "recipe[users::sysadmins]",
-  "recipe[sudo]"
+  "recipe[sudo]",
+  "recipe[apt]",
+  "recipe[git]",
+  "recipe[build-essential]"
 )
 override_attributes(
   :authorization => {
